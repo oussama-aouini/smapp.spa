239e0aa88977624429ba1623e3e280b69c78868f
Ousaama Aouini <oussama.aouini.dev@gmail.com>
2024-05-18 15:47:20 +0100

    setup react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './components/Home'
+import Profile from './components/Profile'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/profile' element={<Profile />} />
+    </Routes>
   )
 }
 
